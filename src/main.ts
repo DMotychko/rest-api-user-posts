23ea7e983d5e77a6885f2eef8745c5aa18aa88c0
Denis Motichko <motichkodenis10@gmail.com>
2025-02-11 12:45:29 +0100

    added server host part 5

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express, {NextFunction, Request, Response} from "express"
 import swaggerUi from "swagger-ui-express"
+import cors from 'cors'
 import {ApiError} from "./errors/api-error";
 import {config} from "./configs/config";
 import mongoose from "mongoose";
@@ -9,8 +10,10 @@ import {postRouter} from "./routers/post.router";
 import swaggerDocument from '../docs/swagger.json'
 
 
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -18,7 +21,7 @@ app.use('/auth', authRouter )
 app.use('/users', userRouter)
 app.use('/posts', postRouter)
 
-const SERVER_URL = process.env.SERVER_URL || "http://localhost:3000";
+const SERVER_URL = process.env.SERVER_URL || "localhost:3000";
 
 swaggerDocument.host = SERVER_URL
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -33,6 +36,12 @@ app.use('*', (error: ApiError,req: Request, res: Response, next: NextFunction) =
 
     res.status(status).json({status, message})
 })
+
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+}));
+
 process.on('uncaughtException', (error: Error) => {
     console.error("Uncaught Exception: ", error);
     process.exit(1);
